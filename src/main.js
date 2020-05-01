7ae8342ca0ff1b4e4013fa819ece42b94970647b
RamonL <46079563+Pbluer@users.noreply.github.com>
2020-05-01 20:59:41 -0300

    0.0.1
    Projeto iniciado com mobile-frist.

---
+++ src/main.js
@@ -0,0 +1,6 @@
+valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + ( ( diasFerias * diasEfetivos * horasDiarias ) )
+
+diasEfetivos = dias
+horasDiarias = horas
+diasFerias = ferias
+valorHora = total
